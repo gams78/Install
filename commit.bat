04b8472eef5d080cffd5ff318b30e3e7ab4bad06
Boris Volarič <boris@kalvarija.net>
2024-05-06 12:39:18 +0200

    Dodana commit avtomatizacija

---
+++ commit.bat
@@ -0,0 +1,10 @@
+@Echo off
+@Echo Skripta za nalaganje kode na github
+@Echo Copyright (c) by Boris Volaric, TRI-ING d.o.o.
+git status
+set /p message="Opis:"
+git commit -a -m "%message%"
+pause
+git push -u origin master
+@Echo Skripta koncana.
+pause
